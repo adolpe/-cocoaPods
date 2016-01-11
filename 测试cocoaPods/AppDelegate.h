896557dd03d0a62d48503da336156efc696a5f27
scsys <adope@wangwenqiang.local>
2016-01-11 18:56:27 +0800

    Initial Commit

---
+++ 测试cocoaPods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  测试cocoaPods
+//
+//  Created by 王文强 on 16/1/11.
+//  Copyright © 2016年 王文强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 测试cocoaPods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  测试cocoaPods
+//
+//  Created by 王文强 on 16/1/11.
+//  Copyright © 2016年 王文强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
